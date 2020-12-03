fea861701260e6162891fe6c86b8bcfd8d551b40
IgnacioArbol <ignacioarbol@gmail.com>
2020-12-04 00:37:16 +0100

    funciona pero con algunos errores

--- pong.js
+++ pong.js
@@ -1,6 +1,7 @@
 //variables
 var svg;
-
+var pai;
+var pad;
 //funciones
 
 function aleatorio(min, max) {
@@ -11,7 +12,11 @@ function repite(){
     bola.mueve();
     bola.dibuja();
     bola.gol();
-    
+    game.pintar();
+    game.mover();
+    pai.pintar();
+    pad.pintar();
+   
     
 }
 
@@ -31,13 +36,52 @@ class game{
         this.ganadas2=ganadas2;
     }
 
-jugar(){
-    bola= new bola(aleatorio(50,950),aleatorio(3,5),aleatorio(3,5),svg);
-    var palo1= new palo("palo1",50,350,svg);
-    var palo2= new palo("palo2",2450,350,svg);
+    jugar(){
+        bola= new bola(aleatorio(50,950),aleatorio(2,3),aleatorio(3,5),svg);
+         pai= new palo("palo1",50,350,svg);
+         pad= new palo("palo2",2450,350,svg);
     
     
-}
+    
+    }
+    mover(){
+        document.addEventListener("keydown",(e) =>{
+            switch(e.key){
+                case 'w':
+                    pai.moverArriba();
+                    break;
+                case 's':
+                    pai.moverAbajo();
+                    break;
+                case 'ArrowUp':
+                    pad.moverArriba();
+                    break;
+                case 'ArrowDown':
+                    pad.moverAbajo();
+                    break;
+            }
+        })
+        /*/
+        document.addEventListener("keydown", event=>{
+        if(event.key=="w" || event.key=="W"){
+            pai.moverArriba();
+        }
+        if(event.key=="s"|| event.key=="S"){
+            pai.moverAbajo();
+        }
+        if(event.key=="ArrowUp");{
+            pad.moverArriba();
+        }
+        if(event.key=="ArrowDown"){
+            pad.moverAbajo();
+        }
+        });
+        /*/
+    }
+    pintar(){
+        document.getElementById("text1").innerHTML = this.ganadas1;
+        document.getElementById("text2").innerHTML = this.ganadas2;
+    }
 
 }
 
@@ -74,28 +118,32 @@ class bola {
         var p2= document.getElementById("palo2");
         
         if(this.x >=p2.x.animVal.value-this.radio && this.x <=p2.x.animVal.value+p2.width.animVal.value && this.y+this.radio>=p2.y.animVal.value && this.y-this.radio<=(p2.y.animVal.value+p2.height.animVal.value)){
-            this.velx = -1;
-            this.velx  -=2;
+            this.velx *= -1;
+            if(this.velx>=-20){
+                this.velx  -=2;
+            }
             if(Math.round(Math.random())*3==3){
                 if(this.vely>0){
                     this.vely+=1;
-                    if(this.vely>10)this.vely=9;
+                    if(this.vely>10)this.vely=5;
                 }else{
                     this.vely+=-1;
-                    if(this.vely<-10)this.vely=-9;
+                    if(this.vely<-10)this.vely=-5;
                  }
             }
         }
         if(this.x-this.radio<=p1.x.animVal.value+p1.width.animVal.value && this.x >=p1.x.animVal.value && this.y+this.radio>=p1.y.animVal.value && this.y-this.radio<=p1.y.animVal.value+p1.height.animVal.value){
-            this.velx = -1;
-            this.velx+=3;
+            this.velx *= -1;
+            if(this.velx<=20){
+            this.velx  +=3;
+            }
             if(Math.round(Math.random())*3==3){
                 if(this.vely>0){
                     this.vely+=1;
-                    if(this.vely>10)this.vely=6;
+                    if(this.vely>10)this.vely=3;
                 }else{
                     this.vely+=-1;
-                    if(this.vely<-10)this.vely=-6;
+                    if(this.vely<-10)this.vely=-3;
                 }
             }
         }   
@@ -105,7 +153,22 @@ class bola {
         this.tagcircle.setAttributeNS(null, "cy", this.y);
     }
     gol(){
+        if(this.x>=2450){
+            game.ganadas1++;
+            this.x= 1250;
+            this.velx=aleatorio(3,5);
+            this.vely=aleatorio(-3,3);
+            
+            
+        }
+        if(this.x<=45){
+            game.ganadas2++;
+            this.x= 1250;
+            this.velx=aleatorio(-5,-3);
+            this.vely=aleatorio(-3,3);
 
+            
+        }
     }
 }
 
@@ -115,7 +178,7 @@ class palo{
         this.x=x;
         this.y=y;
         this.width=20;
-        this.height=150;
+        this.height=180;
         this.nombre=nombre;
 
         this.tagpalo=document.createElementNS("http://www.w3.org/2000/svg","rect");
@@ -127,6 +190,7 @@ class palo{
         this.tagpalo.setAttributeNS(null,"fill","white");
         svgcontenedor.appendChild(this.tagpalo);
         
+        /*/
         var p1= document.getElementById("palo1");
         var p2= document.getElementById("palo2");
         
@@ -155,9 +219,16 @@ class palo{
                     p2.setAttributeNS(null,"y",p2.y.animVal.value+50);
                 }
                 }
-            })
-        
-        
-}
+            })/*/
+        }
+        moverArriba(){
+            this.y=this.y-0.1;
+        }
+        moverAbajo(){
+            this.y=this.y+0.1;
+        }
+        pintar(){
+            this.tagpalo.setAttributeNS(null,"y",this.y);
+        }
 
 }
